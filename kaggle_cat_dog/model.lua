dc97924bf868176c4ede48f3e18de94971945453
Zhou Chang <achang.zhou@qq.com>
2015-08-04 17:17:19 +0800

    More deeper model get about 93%
    Signed-off-by: Zhou Chang <achang.zhou@qq.com>

--- kaggle_cat_dog/model.lua
+++ kaggle_cat_dog/model.lua
@@ -17,17 +17,19 @@ model:add(nn.ReLU())
 model:add(nn.SpatialMaxPooling(2, 2))
 model:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1))
 model:add(nn.ReLU())
+model:add(nn.SpatialConvolution(128, 128, 3, 3, 1, 1, 1))
+model:add(nn.ReLU())
 model:add(nn.SpatialMaxPooling(2, 2))
 model:add(nn.Reshape(128*4*4))
 model:add(nn.Dropout(0.5))
-model:add(nn.Linear(128*4*4, 2048))
+model:add(nn.Linear(2048, 2048))
+model:add(nn.Dropout(0.5))
 model:add(nn.ReLU())
 model:add(nn.Linear(2048, 2048))
 model:add(nn.Dropout(0.5))
 model:add(nn.ReLU())
 model:add(nn.Linear(2048, 1))
 model:add(nn.Sigmoid())
-
 return model
 
 
